f321c25295c424839bfa1d2a8ce3444f350b7cd4
Benjamin Dehlin <ben.dehlin@gmail.com>
2020-06-25 03:23:05 -0600

    full sequelize auth controller, index refactored

--- server/config/database.js
+++ server/config/database.js
@@ -2,7 +2,7 @@ require("dotenv").config({ path: __dirname + "/../.env" })
 const Sequelize = require("sequelize")
 const { CONNECTION_STRING } = process.env
 module.exports = new Sequelize(CONNECTION_STRING, {
-  dialect: "posgres",
+  dialect: "postgres",
   dialectOptions: {
     ssl: { require: true, rejectUnauthorized: false },
   },
--- server/index.js
+++ server/index.js
@@ -1,22 +1,17 @@
 require("dotenv").config({ path: __dirname + "/../.env" })
 const express = require("express")
-const session = require("express-session")
-// const massive = require("massive")
 const app = express()
-
 
 const { SERVER_PORT, SESSION_SECRET } = process.env
 
 //Controllers
 const authCtrl = require("./controllers/authController")
-const sqlCtrl = require('./controllers/sequelizeController')
-const newAuthCtrl = require('./controllers/newAuthController')
 
 //Middleware
 const authMid = require("./middleware/authMiddleware")
 app.use(express.json())
 app.use(
-  session({
+  require("express-session")({
     secret: SESSION_SECRET,
     resave: false,
     saveUninitialized: true,
@@ -25,31 +20,24 @@ app.use(
 )
 
 //Database connection
-const db = require('./config/database')
-app.set('db', db)
-const User = require('./models/User')
+const db = require("./config/database")
+const User = require("./models/User")
+app.set("db", db)
 app.set("User", User)
-db.authenticate().then(() => {
-  console.log('Connection Established')
-  app.listen(SERVER_PORT, console.log(`Server listening on ${SERVER_PORT}`))
-}).catch(err => console.log(err))
-// massive({
-//   connectionString: CONNECTION_STRING,
-//   ssl: { rejectUnauthorized: false },
-// }).then((db) => {
-//   app.set("db", db)
-//   console.log("Database connected")
-//   const io = require("socket.io")(
-//     app.listen(SERVER_PORT, () =>
-//       console.log(`Server listening on ${SERVER_PORT}`)
-//     )
-//   )
-//   app.set("io", io)
-//   //Socket connection
-//   io.on("connection", (socket) => {
-//     console.log("User connected")
-//   })
-// })
+db.authenticate()
+  .then(() => {
+    console.log("Connection Established")
+    const io = require("socket.io")(
+      app.listen(SERVER_PORT, () =>
+        console.log(`Server listening on ${SERVER_PORT}`)
+      )
+    )
+    app.set("io", io)
+    io.on("connection", (socket) => {
+      console.log("User connected")
+    })
+  })
+  .catch((err) => console.log(err))
 
 //Endpoints
 //Auth Endpoints
@@ -57,9 +45,3 @@ app.post("/auth/register", authCtrl.register)
 app.post("/auth/login", authCtrl.login)
 app.post("/auth/logout", authCtrl.logout)
 app.get("/auth/user", authMid.usersOnly, authCtrl.getUser)
-
-//Sequelize Endpoints
-app.get('/api/test', sqlCtrl.test)
-app.get('/api/test2', sqlCtrl.testTwo)
-app.post('/api/test3', sqlCtrl.testThree)
-app.post('/auth/registertest', newAuthCtrl.register)
